9c256d44bb8ca8b1d4c7197a45c16857eb359810
BlueZeeKing <github@blueish.dev>
2022-06-25 19:33:12 -0400

    Remove unneeded imports

--- src/main/java/dev/blueish/origins/swap/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/dev/blueish/origins/swap/mixin/ServerPlayerEntityMixin.java
@@ -2,16 +2,11 @@ package dev.blueish.origins.swap.mixin;
 
 import dev.blueish.origins.swap.OriginsSwap;
 import io.github.apace100.origins.component.OriginComponent;
-import io.github.apace100.origins.networking.ModPackets;
-import io.github.apace100.origins.networking.ModPacketsC2S;
 import io.github.apace100.origins.origin.*;
 import io.github.apace100.origins.registry.ModComponents;
 import io.netty.buffer.Unpooled;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.network.PacketByteBuf;
-import net.minecraft.network.packet.s2c.play.PlayerRespawnS2CPacket;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
